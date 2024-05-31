5690001129e54957953df7a0357ca193af285f56
leftoverlabs <leftoverlabs@proton.me>
2024-05-31 18:39:49 +0300

    Update moisture.py

--- moisture.py
+++ moisture.py
@@ -1,20 +1,34 @@
-# import required modules
-from machine import ADC, Pin
-import utime
- 
-# use variables instead of numbers:
-soil = ADC(Pin(26)) # Soil moisture PIN reference
- 
-#Calibraton values
-min_moisture=19200
-max_moisture=49300
- 
-readDelay = 0.5 # delay between readings
- 
+import time
+import spidev
+import RPi.GPIO as GPIO
+
+# AD/DA channel on MCP3008
+soil_channel = 0
+
+# Open SPI bus
+spi = spidev.SpiDev()
+spi.open(0, 0)
+
+
+# Read SPI data from MCP3008, Channel must be an integer 0-7
+def ReadADC(channel):
+    adc = spi.xfer2([1, (8 + channel) << 4, 0])
+    data = ((adc[1] & 3) << 8) + adc[2]
+    return data
+
+
+# Calibration values 
+min_moisture = 19200
+max_moisture = 49300
+
+readDelay = 0.5  # delay between readings
+
 while True:
     # read moisture value and convert to percentage into the calibration range
-    moisture = (max_moisture-soil.read_u16())*100/(max_moisture-min_moisture) 
+    raw_data = ReadADC(soil_channel)
+    moisture = (max_moisture - raw_data) * 100 / (max_moisture - min_moisture)
+
     # print values
-    print("moisture: " + "%.2f" % moisture +"% (adc: "+str(soil.read_u16())+")")
-    
-    utime.sleep(readDelay) # set a delay between readings    
+    print("moisture: " + "%.2f" % moisture + "% (adc: " + str(raw_data) + ")")
+
+    time.sleep(readDelay)  # set a delay between readings
