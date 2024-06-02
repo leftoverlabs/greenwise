0aed4d14662518f3a417df167cb5a5932900c43e
leftoverlabs <leftoverlabs@proton.me>
2024-06-02 13:51:00 +0300

    Update setup_greenwise_env.sh

--- setup_greenwise_env.sh
+++ setup_greenwise_env.sh
@@ -1,28 +1,26 @@
 #!/bin/bash
 
-# Update package list
-sudo apt-get update
+# define location where the virtual environment will be created
+PROJECT_DIR=~/greenwise
 
-# Check whether virtualenv module is installed
+# Check if the virtualenv module is installed
 virtualenv --version &>/dev/null
 if [ $? -ne 0 ]; then
     echo "virtualenv not found. Installing virtualenv..."
     sudo apt-get install -y python3-virtualenv
 fi
 
-# Check whether virtualenvwrapper module is installed
-source `which virtualenvwrapper_lazy.sh` &>/dev/null
+# Check if the virtualenvwrapper module is installed
+source /usr/share/virtualenvwrapper/virtualenvwrapper_lazy.sh &>/dev/null
 if [ $? -ne 0 ]; then
     echo "virtualenvwrapper not found. Installing virtualenvwrapper..."
     sudo apt-get install -y virtualenvwrapper
 fi
 
-# Ensure that virtualenvwrapper is loaded on shell startup
-echo "source `which virtualenvwrapper_lazy.sh`" >> ~/.bashrc
-# Apply the change immediately for this script
-source ~/.bashrc
+# Source the virtualenvwrapper_lazy.sh script
+source /usr/share/virtualenvwrapper/virtualenvwrapper_lazy.sh
 
 # Create a new virtual environment named greenwise
-mkvirtualenv greenwise --python=$(which python3)
+mkvirtualenv --python=$(which python3) greenwise -a $PROJECT_DIR
 
-echo "You can now activate the greenwise environment using 'workon greenwise'. To deactivate, use 'deactivate'."
+echo "You can now activate the greenwise environment (which is now associated with your project) using 'workon greenwise'. To deactivate, use 'deactivate'."
