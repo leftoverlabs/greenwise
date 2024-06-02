b5b5b2310eee2b129965587fc08e74d6730d86ac
leftoverlabs <leftoverlabs@proton.me>
2024-06-02 13:04:10 +0300

    Update setup_hat_mini.sh

--- setup_hat_mini.sh
+++ setup_hat_mini.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
+# install_automation_hat.sh
 
-# Clone the repository
-git clone https://github.com/pimoroni/automation-hat
+# Define the installation directory
+dir="~/automation-hat"
+
+# Clone the repository into the defined directory
+git clone https://github.com/pimoroni/automation-hat $dir
 
 # Move into the cloned directory
-cd automation-hat
+cd $dir
 
 # Run the installer
 ./install.sh
@@ -14,10 +18,10 @@ sudo raspi-config nonint do_i2c 0
 sudo raspi-config nonint do_spi 0
 
 # Add the activation command to bashrc file to create an alias
-echo "alias activate_automationhat='source ~/.virtualenvs/pimoroni/bin/activate'" >> ~/.bashrc
+echo "alias activate_ah='source ~/.virtualenvs/pimoroni/bin/activate'" >> ~/.bashrc
 
 # Add the deactivation command to bashrc to create an alias
-echo "alias deactivate_automationhat='deactivate'" >> ~/.bashrc
+echo "alias deactivate_ah='deactivate'" >> ~/.bashrc
 
 # Use source command to apply changes immediately
 source ~/.bashrc
