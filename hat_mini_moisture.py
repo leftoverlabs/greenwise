2f94bb63bc545605fa98c010f749f2c675ee1958
leftoverlabs <hello@leftoverlabs.com>
2024-09-03 21:20:14 +0300

    Update hat_mini_moisture.py

--- hat_mini_moisture.py
+++ hat_mini_moisture.py
@@ -10,7 +10,11 @@ Press CTRL+C to exit.
 
 # Create ST7735 LCD display class.
 disp = st7735.ST7735(
-	@@ -25,28 +18,44 @@
+    port=0,
+    cs=st7735.BG_SPI_CS_FRONT,
+    dc=9,
+    backlight=25,
+    rotation=270,
     spi_speed_hz=4000000
 )
 
