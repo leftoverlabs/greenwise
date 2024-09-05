52e1ef5d32974c6c2969d24d2da208231922c03f
leftoverlabs <hello@leftoverlabs.com>
2024-09-05 19:32:29 +0300

    Update greenwise_env_setup.sh

--- greenwise_env_setup.sh
+++ greenwise_env_setup.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-# define location where the virtual environment will be created
+# Define location where the virtual environment will be created
 PROJECT_DIR=~/greenwise
 
 # Update package list
-# sudo apt-get update
+sudo apt-get update
+
 # Enable SPI
 sudo raspi-config nonint do_spi 0
 
@@ -18,35 +19,43 @@ if ! command -v virtualenv >/dev/null; then
 fi
 
 # Install virtualenvwrapper if needed
-if ! command -v virtualenvwrapper >/dev/null; then
+if ! command -v virtualenvwrapper.sh >/dev/null; then
     echo "virtualenvwrapper not found. Installing virtualenvwrapper..."
     sudo apt-get install -y virtualenvwrapper
 fi
 
 # Source the virtualenvwrapper script and set environment variables
 if ! grep -q 'export WORKON_HOME=~/.virtualenvs' ~/.bashrc ; then
-	echo 'export WORKON_HOME=~/.virtualenvs' >> ~/.bashrc
+    echo 'export WORKON_HOME=~/.virtualenvs' >> ~/.bashrc
 fi
 
 VIRTUALENVWRAPPER_PYTHON=$(which python3)
 if ! grep -q "export VIRTUALENVWRAPPER_PYTHON=$VIRTUALENVWRAPPER_PYTHON" ~/.bashrc ; then
-	 echo "export VIRTUALENVWRAPPER_PYTHON=$VIRTUALENVWRAPPER_PYTHON" >> ~/.bashrc
+    echo "export VIRTUALENVWRAPPER_PYTHON=$VIRTUALENVWRAPPER_PYTHON" >> ~/.bashrc
 fi
 
 if ! grep -q 'source /usr/share/virtualenvwrapper/virtualenvwrapper.sh' ~/.bashrc ; then
-	echo 'source /usr/share/virtualenvwrapper/virtualenvwrapper.sh' >> ~/.bashrc
+    echo 'source /usr/share/virtualenvwrapper/virtualenvwrapper.sh' >> ~/.bashrc
 fi
 
-# reload .bashrc so that changes take effect in the current session
+# Reload .bashrc so that changes take effect in the current session
 source ~/.bashrc
 
-# Create a new virtual environment named greenwise
-mkvirtualenv greenwise -a ~/greenwise -p $VIRTUALENVWRAPPER_PYTHON
+# Debug print statements to check if paths are set correctly
+echo "WORKON_HOME is set to $WORKON_HOME"
+echo "VIRTUALENVWRAPPER_PYTHON is set to $VIRTUALENVWRAPPER_PYTHON"
+echo "virtualenvwrapper.sh is sourced"
 
+# Source the virtualenvwrapper script directly to ensure it's available
+source /usr/share/virtualenvwrapper/virtualenvwrapper.sh
+
+# Create a new virtual environment named greenwise
+mkvirtualenv greenwise -a "$PROJECT_DIR" -p "$VIRTUALENVWRAPPER_PYTHON"
+
+# Workon new virtual environment
 workon greenwise
 
-pip install pillow
-pip install automationhat
-pip install st7735
+# Install necessary packages
+pip install pillow automationhat st7735
 
 echo "You can now activate the greenwise environment using 'workon greenwise'. To deactivate, use 'deactivate'."
