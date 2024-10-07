fdb10b9278fe2442c1574ec5eb983225200365ba
leftoverlabs <hello@leftoverlabs.com>
2024-10-07 23:16:01 +0300

    Create st7735_part1.py

---
+++ code_snippets/st7735_part1.py
@@ -0,0 +1,14 @@
+import st7735
+
+# Create ST7735 LCD display class.
+disp = st7735.ST7735(
+    port=0,
+    cs=st7735.BG_SPI_CS_FRONT,
+    dc=9,
+    backlight=25,
+    rotation=270,
+    spi_speed_hz=4000000
+)
+
+# Initialize display.
+disp.begin()
