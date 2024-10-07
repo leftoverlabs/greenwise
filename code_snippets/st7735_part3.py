c8ec4354df3dec83ffbb61dd8440e1d2b22e965f
leftoverlabs <hello@leftoverlabs.com>
2024-10-07 23:19:28 +0300

    Create st7735_part3.py

---
+++ code_snippets/st7735_part3.py
@@ -0,0 +1,4 @@
+# Now to display some text on the screen we can use the code below
+draw.rectangle((0, 0, WIDTH, HEIGHT), (0, 0, 0))  # Used to clear the screen
+draw.text((10, 10), 'Hello World', fill=(255, 255, 255))  # Draw "Hello World" 
+disp.display(img)  # Display the new image with the text on the screen.
