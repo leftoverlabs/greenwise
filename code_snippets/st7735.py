40a5a3fff09a43d914909d3796e737ac67221799
leftoverlabs <hello@leftoverlabs.com>
2024-10-07 23:05:15 +0300

    Update st7735.py

--- code_snippets/st7735.py
+++ code_snippets/st7735.py
@@ -24,3 +24,8 @@ img = Image.new('RGB', (WIDTH, HEIGHT), color=(0, 0, 0))
 draw = ImageDraw.Draw(img)
 
 disp.display(img)
+
+# Now to display some text on the screen we can use the code below
+draw.rectangle((0, 0, WIDTH, HEIGHT), (0, 0, 0))  # Used to clear the screen
+draw.text((10, 10), 'Hello World', fill=(255, 255, 255))  # Draw "Hello World" 
+disp.display(img)  # Display the new image with the text on the screen.
