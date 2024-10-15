f75025c4f8e3bfd3901e556e08baa7bc283a3d32
leftoverlabs <hello@leftoverlabs.com>
2024-10-15 22:21:24 +0300

    Update output_humidity_percentage_to_automation_hat_mini.py

--- code_snippets/output_humidity_percentage_to_automation_hat_mini.py
+++ code_snippets/output_humidity_percentage_to_automation_hat_mini.py
@@ -1,7 +1,6 @@
 while True:
     adc_value = automationhat.analog.one.read()
     humidity_percentage = int(convert_to_humidity(humidity_adc_value))
-
     draw.rectangle((0, 0, WIDTH, HEIGHT), (0, 0, 0))  # Clear the screen
     draw.text((10, 10), f'Humidity: {humidity_percentage}%', fill=(255, 255, 255))
     disp.display(img)
