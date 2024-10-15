59518ac08a0f53b09eca09b2bad359b396d2f469
leftoverlabs <hello@leftoverlabs.com>
2024-10-15 22:09:55 +0300

    Update converting_humidity_adc_value_to_percentage.py

--- code_snippets/converting_humidity_adc_value_to_percentage.py
+++ code_snippets/converting_humidity_adc_value_to_percentage.py
@@ -1,1 +1,6 @@
+def convert_to_humidity(analog_value):
+    min_value = 1.05 
+    max_value = 2.09 
+    return 100 - ((analog_value - min_value) / (max_value - min_value)) * 100.0
+
 humidity_percentage = int(convert_to_humidity(humidity_adc_value))
